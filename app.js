10d3a4073da3ca4ed92a8e578ae0b80f34323be1
Alquimista <jcguadamuz2913@gmail.com>
2026-09-17 20:46:01 -0600

    Registro Configuracion

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const APP_NAME = 'KSA PRÁCTIKA';
-  const APP_VERSION = '0.18.99-facturas-manuales';
+  const APP_VERSION = '0.18.100-cobros-ultimo-registro';
   const SCHEMA_VERSION = '1.0.0';
   const STORAGE_KEY = 'KSA_PRACTIKA_DATA_v1';
   const DEVICE_IDENTITY_STORAGE_KEY = 'KSA_PRACTIKA_DEVICE_IDENTITY_v1';
