cd7a7c8e7f314eedcde53e010fceddcf966013d0
Alquimista <jcguadamuz2913@gmail.com>
2026-09-25 10:15:50 -0600

    Mejora Visual Ventas

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_VERSION = 'v0_18_106_mejora_visual_gastos_ajustes';
+const CACHE_VERSION = 'v0_18_107_mejora_visual_ventas';
 const CACHE_NAME = `KSA_PRACTIKA_CACHE_${CACHE_VERSION}`;
 const APP_SHELL = [
   './',
